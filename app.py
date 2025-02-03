6dc364ec5b42b83538bad083c8bc2b647ece2df2
joelsinhox <joelsinhox@gmail.com>
2025-02-03 15:21:26 -0300

    add canais

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-from flask import Flask, jsonify, request, make_response, render_template
+from flask import Flask, jsonify, request, make_response, render_template, Response
 from netcine import catalog_search, search_link
+import json
+import requests
 
 app = Flask(__name__)
 
@@ -12,29 +14,52 @@ def add_cors_headers(response):
 
 
 MANIFEST = {
-        "id": "com.skyflix",
-        "version": "1.0.0",
-        "name": "SKYFLIX",
-        "description": "Tenha o melhor dos filmes e series com skyflix",
-        'logo': 'https://i.imgur.com/qVgkbYn.png',
-        "resources": ["catalog", "stream"],
-        "types": ["movie", "series"],
-        "catalogs": [
-            {
-                "type": "movie",
-                "id": "skyflix",
-                "name": "IMDB",
-                "extraSupported": ["search"]
-            },
-            {
-                "type": "series",
-                "id": "skyflix",
-                "name": "IMDB",
-                "extraSupported": ["search"]
-            }
-        ],
-        "idPrefixes": ["tt"]
-    }
+    "id": "com.skyflix",
+    "version": "1.0.0",
+    "name": "SKYFLIX",
+    "description": "Tenha o melhor dos filmes e séries com Skyflix",
+    "logo": "https://i.imgur.com/qVgkbYn.png",
+    "resources": ["catalog", "meta", "stream"],
+    "types": ["tv", "movie", "series"],
+    "catalogs": [
+        {
+            "type": "tv",
+            "id": "skyflix",
+            "name": "SKYFLIX",
+            "extra": [
+                {
+                    "name": "genre",
+                    "options": [
+                        "Canais Abertos",
+                        "Variedades",
+                        "Filmes e Series",
+                        "Documentarios",
+                        "Esportes",
+                        "Infantil",
+                        "Noticias",
+                        "PLUTO TV",
+                        "CANAL 24H"
+                    ],
+                    "isRequired": False
+                }
+            ]
+        },
+        {
+            "type": "movie",
+            "id": "skyflix",
+            "name": "SKYFLIX",
+            "extraSupported": ["search"]
+        },
+        {
+            "type": "series",
+            "id": "skyflix",
+            "name": "SKYFLIX",
+            "extraSupported": ["search"]
+        }
+    ],
+    "idPrefixes": ["skyflix", "tt"]
+}
+
 
 @app.route('/')
 def home():
@@ -45,18 +70,75 @@ def home():
     version = MANIFEST['version']
     return render_template('index.html', name=name, types=types, logo=logo, description=description, version=version)
 
+@app.route('/logo')
+def proxy_logo():
+    image_url = request.args.get('url')
+
+    if not image_url:
+        return "Erro: Nenhuma URL fornecida", 400
+
+    try:
+        response = requests.get(image_url, stream=True)
+
+        if response.status_code != 200:
+            return f"Erro ao buscar a imagem: {response.status_code}", 400
+
+        content_type = response.headers.get('Content-Type', 'image/jpeg')
+
+        # Criar a resposta e adicionar os headers CORS manualmente
+        resp = Response(response.content, content_type=content_type)
+        resp.headers['Access-Control-Allow-Origin'] = '*'  # Permite acesso de qualquer origem
+        resp.headers['Access-Control-Allow-Methods'] = 'GET, OPTIONS'  # Métodos permitidos
+        resp.headers['Access-Control-Allow-Headers'] = 'Content-Type'  # Permite cabeçalhos específicos
+
+        return resp
+
+    except requests.exceptions.RequestException as e:
+        return f"Erro ao buscar a imagem: {str(e)}", 500
+
 # Rota para o manifesto do addon
 @app.route('/manifest.json')
 def manifest():
     response = jsonify(MANIFEST)
     return add_cors_headers(response)
 
+@app.route('/catalog/tv/skyflix/genre=<id>.json')
+def genres(id):
+    host = request.host
+    if 'localhost' in host or '127.0.0.1' in host:
+        server = f'http://{host}/logo?url='
+    else:
+        server = f'https://{host}/logo?url='
+    try:
+        r = requests.get(f'https://oneplayhd.com/stremio_oneplay/catalog/tv/OnePlay/genre={id}.json').text
+        r = r.replace('oneplay:', 'skyflix:')
+        r = r.replace('https', server+'https')
+        data = json.loads(r)
+        response = jsonify(data) 
+    except:
+        response = jsonify({
+        "metas": []
+        })
+    return add_cors_headers(response)
+
 # Rota para o catálogo
 @app.route('/catalog/<type>/<id>.json')
 def catalog_route(type, id):
-    response = jsonify({
+    host = request.host
+    if 'localhost' in host or '127.0.0.1' in host:
+        server = f'http://{host}/logo?url='
+    else:
+        server = f'https://{host}/logo?url='      
+    if type == 'tv':
+        r = requests.get('https://oneplayhd.com/stremio_oneplay/catalog/tv/OnePlay.json').text
+        r = r.replace('oneplay:', 'skyflix:')
+        r = r.replace('https', server+'https')
+        data = json.loads(r)
+        response = jsonify(data)
+    else:
+        response = jsonify({
         "metas": []
-    })
+        })
     return add_cors_headers(response)
 
 # Rota para pesquisa
@@ -72,28 +154,57 @@ def search(type, query):
     })
     return add_cors_headers(response)
 
+@app.route('/meta/<type>/<id>.json')
+def meta(type,id):
+    host = request.host
+    if 'localhost' in host or '127.0.0.1' in host:
+        server = f'http://{host}/logo?url='
+    else:
+        server = f'https://{host}/logo?url='      
+    if type == 'tv':
+        id_channels = id.split(':')[1]
+        r = requests.get(f'https://oneplayhd.com/stremio_oneplay/meta/tv/oneplay:{id_channels}.json').text
+        r = r.replace('oneplay:', 'skyflix:')
+        r = r.replace('https', server+'https')
+        data = json.loads(r)
+        response = jsonify(data)
+    else:
+        response = jsonify({
+        "meta": {}
+        })
+    return add_cors_headers(response)
+           
+
 # Rota para streams (exemplo simples)
 @app.route('/stream/<type>/<id>.json')
 def stream(type, id):
-    try:
-        stream_, headers = search_link(id)
-        scrape_ = [{
-                "url": stream_,
-                "name": "SKYFLIX",
-                "description": "Netcine",
-                "behaviorHints": {
-                    "notWebReady": True,
-                    "proxyHeaders": {
-                        "request": {
-                            "User-Agent": headers['User-Agent'],
-                            "Referer": headers['Referer'],
-                            "Cookie": headers['Cookie']
+    if type == 'tv':
+        id_channels = id.split(':')[1]
+        url = f'https://oneplayhd.com/stremio_oneplay/stream/tv/oneplay:{id_channels}.json'
+        r = requests.get(url).json()
+        scrape_ = r.get('streams', []) 
+    elif type == 'movie' or type == 'series':
+        try:
+            stream_, headers = search_link(id)
+            scrape_ = [{
+                    "url": stream_,
+                    "name": "SKYFLIX",
+                    "description": "Netcine",
+                    "behaviorHints": {
+                        "notWebReady": True,
+                        "proxyHeaders": {
+                            "request": {
+                                "User-Agent": headers['User-Agent'],
+                                "Referer": headers['Referer'],
+                                "Cookie": headers['Cookie']
+                            }
                         }
                     }
-                }
-            }]
-    except:
-        scrape_ = []   
+                }]
+        except:
+            scrape_ = []
+    else:
+        scrape_ = []  
     response = jsonify({
         "streams": scrape_
     })
