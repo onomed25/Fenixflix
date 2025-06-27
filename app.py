0759f8aea5e42ce768532ed1b4f327b5c0650ff9
joelsinhox <joelsinhox@gmail.com>
2025-06-26 21:26:17 -0300

    fix

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import RedirectResponse
-from starlette.middleware.cors import CORS
+from fastapi.middleware.cors import CORS
 
 app = FastAPI()
 
