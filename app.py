7e810bf4e5bcd0d9615cecd47ffad9585dad60ba
joelsinhox <joelsinhox@gmail.com>
2025-06-26 21:22:24 -0300

    migrar host

--- app.py
+++ app.py
@@ -1,212 +1,28 @@
-from fastapi import FastAPI, Request, Response
-from fastapi.responses import JSONResponse, HTMLResponse
-from jinja2 import Environment, FileSystemLoader
-from slowapi import Limiter
-from slowapi.errors import RateLimitExceeded
-from slowapi.util import get_remote_address
-import logging
-from netcine import catalog_search, search_link
-import os
-from urllib.parse import quote_plus, quote, unquote
-import get_channels
-
-templates = Environment(loader=FileSystemLoader("templates"))
-limiter = Limiter(key_func=get_remote_address)
-rate_limit = '3/second'
+from fastapi import FastAPI, Request
+from fastapi.responses import RedirectResponse
+from starlette.middleware.cors import CORS
 
 app = FastAPI()
 
+# Configurar CORS (opcional, caso precise de suporte a cross-origin)
+app.add_middleware(
+    CORS,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-# Configure o logger
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
-VERSION = "1.0.1"
-logger.info(f"Versão da aplicação: {VERSION}")
+# URL de destino para o redirecionamento
+TARGET_URL = "https://zoreu-skyflix.hf.space"
 
-
-
-MANIFEST = {
-    "id": "com.skyflix",
-    "version": "1.0.1",
-    "name": "SKYFLIX",
-    "description": "Tenha o melhor dos filmes e séries com Skyflix",
-    "logo": "https://i.imgur.com/qVgkbYn.png",
-    "resources": ["catalog", "meta", "stream"],
-    "types": ["tv", "movie", "series"],
-    "catalogs": [
-        {
-            "type": "tv",
-            "id": "skyflix",
-            "name": "SKYFLIX",
-            "extra": [
-                {
-                    "name": "genre",
-                    "options": [
-                        "Abertos",
-                        "Reality",
-                        "Esportes",
-                        "NBA",
-                        "PPV",
-                        "Paramount plus",
-                        "DAZN",
-                        "Nosso Futebol",
-                        "UFC",
-                        "Combate",
-                        "NFL",
-                        "Documentarios",
-                        "Infantil",
-                        "Filmes e Series",
-                        "Telecine",
-                        "HBO",
-                        "Cine Sky",
-                        "Noticias",
-                        "Musicas",
-                        "Variedades",
-                        "Cine 24h",
-                        "Desenhos",
-                        "Series 24h",
-                        "Religiosos",
-                        "4K",
-                        "Radios"
-                    ],
-                    "isRequired": False
-                }
-            ]
-        },
-        {
-            "type": "movie",
-            "id": "skyflix",
-            "name": "SKYFLIX",
-            "extraSupported": ["search"]
-        },
-        {
-            "type": "series",
-            "id": "skyflix",
-            "name": "SKYFLIX",
-            "extraSupported": ["search"]
-        }
-    ],
-    "idPrefixes": ["skyflix", "tt"]
-}
-
-# def get_external_ip():
-#     try:
-#         response = requests.get("https://api64.ipify.org?format=json")
-#         return response.json().get("ip")
-#     except requests.RequestException:
-#         return None
-
-@app.exception_handler(RateLimitExceeded)
-async def rate_limit_handler(request, exc):
-    return JSONResponse(content={"error": "Too many requests"}, status_code=429)
-
-def add_cors(response: Response):
-    response.headers["Access-Control-Allow-Origin"] = "*"
-    response.headers["Access-Control-Allow-Methods"] = "GET, POST, OPTIONS"
-    response.headers["Access-Control-Allow-Headers"] = "Content-Type"
-    response.headers["Access-Control-Allow-Credentials"] = "true"
-    return response
-
-@app.get("/", response_class=HTMLResponse)
-@limiter.limit(rate_limit)
-async def home(request: Request):
-    template = templates.get_template("index.html")
-    response = HTMLResponse(template.render(
-        name=MANIFEST['name'],
-        types=MANIFEST['types'],
-        logo=MANIFEST['logo'],
-        description=MANIFEST['description'],
-        version=MANIFEST['version']
-    ))
-    return add_cors(response)
-
-
-@app.get("/manifest.json")
-@limiter.limit(rate_limit)
-async def manifest(request: Request):
-    return add_cors(JSONResponse(content=MANIFEST))
-
-@app.get("/catalog/tv/skyflix/genre={id}.json")
-@limiter.limit(rate_limit)
-async def genres(id: str, request: Request):
-    try:
-        api = get_channels.get_api()
-        canais_ = [canal for canal in api.list_channels(id)]
-    except:
-        canais_ = []
-    return add_cors(JSONResponse(content={"metas": canais_}))
-
-@app.get("/catalog/{type}/{id}.json")
-@limiter.limit(rate_limit)
-async def catalog_route(type: str, id: str, request: Request):    
-    if type == 'tv':
-        try: 
-            api = get_channels.get_api()       
-            itens = [canal for canal in api.list_channels('Abertos')]
-        except:
-            itens = []
-    else:
-        itens = []
-    return add_cors(JSONResponse(content={"metas": itens}))
-
-@app.get("/catalog/{type}/skyflix/search={query}.json")
-@limiter.limit(rate_limit)
-async def search(type: str, query: str, request: Request):
-    catalog = catalog_search(query)
-    results = [item for item in catalog if item.get("type") == type] if catalog else []
-    return add_cors(JSONResponse(content={"metas": results}))
-
-@app.get("/meta/{type}/{id}.json")
-@limiter.limit(rate_limit)
-async def meta(type: str, id: str, request: Request):
-    if type == 'tv':
-        try:
-            m = get_channels.get_meta_tv(id)
-        except:
-            m = {}
-    else:
-        m = {}
-    return add_cors(JSONResponse(content={"meta": m}))
-
-@app.get("/stream/{type}/{id}.json")
-@limiter.limit(rate_limit)
-async def stream(type: str, id: str, request: Request):
-    if type == "tv":
-        scrape_ = []
-        try:
-            scrape_ = get_channels.get_stream_tv(id).get('streams', [])
-        except:
-            pass
-    elif type in ["movie", "series"]:
-        try:
-            stream_, headers = search_link(id)
-            if stream_:
-                scrape_ = [{
-                    "url": stream_,
-                    "name": "SKYFLIX",
-                    "description": "NTC Server",
-                    "behaviorHints": {
-                        "notWebReady": True,
-                        "proxyHeaders": {
-                            "request": {
-                                "User-Agent": headers["User-Agent"],
-                                "Referer": headers["Referer"],
-                                "Cookie": headers["Cookie"]
-                            }
-                        }
-                    }
-                }]
-            else:
-                scrape_ = []
-        except:
-            scrape_ = []
-    else:
-        scrape_ = []
-    return add_cors(JSONResponse(content={"streams": scrape_}))
-
-@app.options("/{path:path}")
-@limiter.limit(rate_limit)
-async def options_handler(path: str, request: Request):
-    return add_cors(Response(status_code=204))
-
-
+@app.api_route("/{path:path}", methods=["GET", "POST"])
+async def redirect_all(request: Request, path: str):
+    # Constrói a URL completa com o path original
+    redirect_url = f"{TARGET_URL}/{path}"
+    
+    # Preserva os query parameters, se houver
+    if request.query_params:
+        redirect_url += f"?{request.query_params}"
+    
+    return RedirectResponse(url=redirect_url, status_code=307)
