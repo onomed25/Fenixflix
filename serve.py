f716cb41b04f92ca213857f58e2338010a28eed3
Onomed <143538592+onomed25@users.noreply.github.com>
2026-02-23 16:28:01 -0300

    Add files via upload

--- serve.py
+++ serve.py
@@ -31,12 +31,14 @@ def search_serve(imdb_id, content_type, season=None, episode=None):
 
                 for stream_obj in stream_objects:
                     if isinstance(stream_obj, dict):
-                        label = stream_obj.get("name") or "Dublado"
+                        # Captura o name e description reais do JSON
+                        nome = stream_obj.get("name") or "FenixFlix"
+                        descricao = stream_obj.get("description") or "Dublado"
                         
                         streams_formatados.append({
-                            "name": "FenixFlix",
+                            "name": nome,
                             "url": stream_obj.get("url"),
-                            "description": label 
+                            "description": descricao 
                         })
                     elif isinstance(stream_obj, str):
                         streams_formatados.append({
@@ -54,12 +56,14 @@ def search_serve(imdb_id, content_type, season=None, episode=None):
             potential_streams = local_data.get('streams', [])
             for stream_obj in potential_streams:
                  if isinstance(stream_obj, dict):
-                    label = stream_obj.get("name") or "Dublado"
+                    # Captura o name e description reais do JSON
+                    nome = stream_obj.get("name") or "FenixFlix"
+                    descricao = stream_obj.get("description") or "Dublado"
 
                     streams_formatados.append({
-                        "name": "FenixFlix",
+                        "name": nome,
                         "url": stream_obj.get("url"),
-                        "description": label
+                        "description": descricao
                     })
                  elif isinstance(stream_obj, str):
                     streams_formatados.append({
