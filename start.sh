ef1732c0c3b759130b0cfeab93e0b7d322e4521e
Onomed <143538592+onomed25@users.noreply.github.com>
2026-03-01 12:24:23 -0300

    Delete start.sh

--- start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Inicia o servidor Go (extrator) em segundo plano
-go run main.go &
-
-# Inicia o servidor Python (API principal) em primeiro plano
-uvicorn app:app --host 0.0.0.0 --port 8000
